b7dbe94931516e0d0d4531df8277addc7aed37ec
Dave Syer <dsyer@vmware.com>
2022-06-30 10:57:16 +0100

    Remove export to allow compiler to work

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,6 +1,15 @@
 @external("env", "console.log")
-declare function print(s: String): void
+declare function print(s: (i: i32) => i32): void
 
-export function main(it: String): void {
-  print(it);
+function increment(input: i32): i32 {
+  return input + 1;
+}
+
+function callback(fn: (input: i32) => i32, input: i32): i32 {
+  return fn(input);
+}
+
+export function call(input: i32): i32 {
+  print(increment);
+  return callback(increment, input);
 }
