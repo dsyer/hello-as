0a76a89fb2dd8a6551c0af47f2fdce70ed09d918
Dave Syer <dsyer@vmware.com>
2022-07-29 12:13:22 +0100

    Migrate more stuff to runtime

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,18 +1,13 @@
 import * as msgpack from '@wapc/as-msgpack/assembly/index';
-import { Buffer, malloc, free } from './runtime';
+import { Buffer, pack, unpack, callback, malloc, free } from './runtime';
 
-export { malloc, free };
+export { malloc, free, callback };
 
 class Message {
   msg: string | null;
 }
 
-function loadMessage(input: Buffer): Message {
-  var result = new Uint8Array(i32(input.len));
-  for (var i = 0; i < i32(input.len); i++) {
-    result[i] = load<u8>(input.data + i);
-  }
-  var decoder = new msgpack.Decoder(result.buffer);
+function decode(decoder:  msgpack.Decoder): Message {
   var msg = new Message();
   decoder.readMapSize();
   decoder.readString();
@@ -27,27 +22,11 @@ function encode(value: Message, writer: msgpack.Writer) : void {
   writer.writeString(value.msg!);
 }
 
-function storeMessage(output: Buffer, value: Message): void {
-  const sizer = new msgpack.Sizer();
-  encode(value, sizer);
-  var bytes = new Uint8Array(sizer.length);
-  encode(value, new msgpack.Encoder(bytes.buffer));
-  output.data = malloc(bytes.byteLength);
-  output.len = bytes.byteLength;
-  for (var i = 0; i < bytes.byteLength; i++) {
-    store<u8>(output.data + i, bytes[i]);
-  }
-}
-
 function greet(output: Buffer, input: Buffer): void {
-  var value = loadMessage(input);
+  var value = unpack(input, decode);
   var result = new Message();
   result.msg = value.msg! + " World";
-  storeMessage(output, result);
-}
-
-export function callback(output: Buffer, fn: i32, input: Buffer): void {
-  call_indirect(fn, output, input);
+  pack(output, result, encode);
 }
 
 export function call(output: Buffer, data: usize, len: usize): void {
--- assembly/runtime.ts
+++ assembly/runtime.ts
@@ -1,3 +1,5 @@
+import * as msgpack from "@wapc/as-msgpack/assembly/index";
+
 export function malloc(size: usize): usize {
   return heap.alloc(size);
 }
@@ -9,6 +11,38 @@ export function free(ptr: usize): void {
 @unmanaged
 export class Buffer {
   constructor() {}
-  data: usize
-  len: usize
+  data: usize;
+  len: usize;
+}
+
+export function callback(output: Buffer, fn: i32, input: Buffer): void {
+  call_indirect(fn, output, input);  
+}  
+
+export function pack<T>(
+  output: Buffer,
+  value: T,
+  encode: (val: T, writer: msgpack.Writer) => void
+): void {
+  const sizer = new msgpack.Sizer();
+  encode(value, sizer);
+  var bytes = new Uint8Array(sizer.length);
+  encode(value, new msgpack.Encoder(bytes.buffer));
+  output.data = malloc(bytes.byteLength);
+  output.len = bytes.byteLength;
+  for (var i = 0; i < bytes.byteLength; i++) {
+    store<u8>(output.data + i, bytes[i]);
+  }
+}
+
+export function unpack<T>(
+  input: Buffer,
+  decode: (decoder: msgpack.Decoder) => T
+): T {
+  var result = new Uint8Array(i32(input.len));
+  for (var i = 0; i < i32(input.len); i++) {
+    result[i] = load<u8>(input.data + i);
+  }
+  var decoder = new msgpack.Decoder(result.buffer);
+  return decode(decoder);
 }
