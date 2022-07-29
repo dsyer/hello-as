f756064b4946194e65726936d587b5bcbc1bc426
Dave Syer <dsyer@vmware.com>
2022-07-29 18:13:31 +0100

    Encapsulate all references to Buffer in a Caller

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,4 @@
-import { Decoder, Writer } from '@wapc/as-msgpack/assembly/index';
-import { Buffer, pack, unpack, callback, malloc, free } from './runtime';
+import { Decoder, Writer, Buffer, Function, callback, malloc, free, Caller } from "./runtime";
 
 export { malloc, free, callback };
 
@@ -7,29 +6,31 @@ class Message {
   msg: string | null;
 }
 
-function decode(decoder:  Decoder): Message {
-  var msg = new Message();
-  decoder.readMapSize();
-  decoder.readString();
-  var value = decoder.readString();  
-  msg.msg = value;
-  return msg;
-}
+class MessageEnhancer implements Function<Message, Message> {
 
-function encode(value: Message, writer: Writer) : void {
-  writer.writeMapSize(1);
-  writer.writeString("msg");
-  writer.writeString(value.msg!);
-}
+  decode(decoder: Decoder): Message {
+    var msg = new Message();
+    decoder.readMapSize();
+    decoder.readString();
+    var value = decoder.readString();
+    msg.msg = value;
+    return msg;
+  }
 
-function greet(output: Buffer, input: Buffer): void {
-  var value = unpack(input, decode);
-  var result = new Message();
-  result.msg = value.msg! + " World";
-  pack(output, result, encode);
+  encode(value: Message, writer: Writer): void {
+    writer.writeMapSize(1);
+    writer.writeString("msg");
+    writer.writeString(value.msg!);
+  }
+
+  apply(value: Message): Message {
+    var result = new Message();
+    result.msg = value.msg! + " World";
+    return result;
+  }
+
 }
 
 export function call(output: Buffer, input: Buffer): void {
-  // Unnecessary virtual call, just to show how it works.
-  callback(output, greet.index, input);
+  new Caller<Message, Message>(new MessageEnhancer()).call(output, input);
 }
--- assembly/runtime.ts
+++ assembly/runtime.ts
@@ -1,4 +1,11 @@
-import * as msgpack from "@wapc/as-msgpack/assembly/index";
+import {
+  Encoder,
+  Decoder,
+  Writer,
+  Sizer,
+} from "@wapc/as-msgpack/assembly/index";
+
+export { Decoder, Writer };
 
 export function malloc(size: usize): usize {
   return heap.alloc(size);
@@ -15,34 +22,46 @@ export class Buffer {
   len: usize;
 }
 
-export function callback(output: Buffer, fn: i32, input: Buffer): void {
-  call_indirect(fn, output, input);  
-}  
+export interface Function<I, O> {
+  apply(input: I): O;
+  encode(output: O, writer: Writer): void;
+  decode(decoder: Decoder): I;
+}
 
-export function pack<T>(
-  output: Buffer,
-  value: T,
-  encode: (val: T, writer: msgpack.Writer) => void
-): void {
-  const sizer = new msgpack.Sizer();
-  encode(value, sizer);
-  var bytes = new Uint8Array(sizer.length);
-  encode(value, new msgpack.Encoder(bytes.buffer));
-  output.data = malloc(bytes.byteLength);
-  output.len = bytes.byteLength;
-  for (var i = 0; i < bytes.byteLength; i++) {
-    store<u8>(output.data + i, bytes[i]);
+export class Caller<I, O> {
+  constructor(fun: Function<I, O>) {
+    this.fun = fun;
+  }
+  private fun: Function<I, O>;
+  public call(output: Buffer, input: Buffer): void {
+    var value = this.unpack(input);
+    var result = this.fun.apply(value);
+    this.pack(output, result);
+  }
+  private pack(
+    output: Buffer,
+    value: O
+  ): void {
+    const sizer = new Sizer();
+    this.fun.encode(value, sizer);
+    var bytes = new Uint8Array(sizer.length);
+    this.fun.encode(value, new Encoder(bytes.buffer));
+    output.data = malloc(bytes.byteLength);
+    output.len = bytes.byteLength;
+    for (var i = 0; i < bytes.byteLength; i++) {
+      store<u8>(output.data + i, bytes[i]);
+    }
+  }
+  private unpack(input: Buffer): I {
+    var result = new Uint8Array(i32(input.len));
+    for (var i = 0; i < i32(input.len); i++) {
+      result[i] = load<u8>(input.data + i);
+    }
+    var decoder = new Decoder(result.buffer);
+    return this.fun.decode(decoder);
   }
 }
 
-export function unpack<T>(
-  input: Buffer,
-  decode: (decoder: msgpack.Decoder) => T
-): T {
-  var result = new Uint8Array(i32(input.len));
-  for (var i = 0; i < i32(input.len); i++) {
-    result[i] = load<u8>(input.data + i);
-  }
-  var decoder = new msgpack.Decoder(result.buffer);
-  return decode(decoder);
+export function callback(output: Buffer, fn: i32, input: Buffer): void {
+  call_indirect(fn, output, input);
 }
